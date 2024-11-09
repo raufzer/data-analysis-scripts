f96832c8dd91260bae6955a9ad18be18d8461a98
raufzer <a_zerkhef@estin.dz>
2024-11-09 22:35:28 +0100

    chore: filitring netflix movies released between 90s & 99s by comparing sub data frame of realse_year colmun with 1990 & 1999

--- script.py
+++ script.py
@@ -6,6 +6,6 @@ import matplotlib.pyplot as plt
 netflix_df = pd.read_csv("netflix_data.csv")
 # print(netflix_df)
 # print(netflix_df[['release_year']])
-filterd_movie_release = netflix_df[(netflix_df[['release_year']] > 1990) &( netflix_df[['release_year']] < 1999)]
+filterd_movie_release = netflix_df[(netflix_df['release_year'] > 1990) &( netflix_df['release_year'] < 1999)]
 print(filterd_movie_release)
 print('test')
