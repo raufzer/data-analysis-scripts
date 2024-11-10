f76c7bb0c5f6f4ee31f0228c1d122a1fc97f506b
raufzer <a_zerkhef@estin.dz>
2024-11-10 21:43:50 +0100

    chore: counting the movies with genre Action during 90s that their duration under 90 min

--- script.py
+++ script.py
@@ -6,11 +6,20 @@ from collections import Counter
 
 # Read in the Netflix CSV as a DataFrame
 netflix_df = pd.read_csv("netflix_data.csv")
-# print(netflix_df)
-# print(netflix_df[['release_year']])
+
 filterd_movie_release = netflix_df[(netflix_df['release_year'] >= 1990) & (netflix_df['release_year'] <= 1999)]
-# print(filterd_movie_release)
+
 durations = filterd_movie_release['duration'].tolist()
 duration = Counter(durations).most_common(1)[0][0]
-print(duration)
-print('test')
+print(f'most frequent movie duration in the 1990s : {duration}')
+
+plt.title('Histogram with the distributions of durations for movies released in the 1990s')
+plt.hist(durations,color='skyblue', alpha=0.7, cumulative=False) 
+
+filterd_movie_genre = filterd_movie_release[filterd_movie_release['genre'] == 'Action']
+short_movie_count = 0
+for lab, row in filterd_movie_genre.iterrows():
+    if row['duration'] <= 90:
+        short_movie_count += 1
+
+print(f'number of short action movies from the 1990s :{short_movie_count}')
